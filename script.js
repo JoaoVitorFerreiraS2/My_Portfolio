6bc59eb44050a269cac1768268136bf26fa5d121
JoaoVitor <joaovitorferreiragoncalves3691@gmail.com>
2024-07-18 21:38:24 -0300

    Atualização do portifólio

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
 console.log('Estamos trabalhando...')
 
+const titulo = document.getElementById('acao');
+
+function typeWrite(elemento) {
+	const textoArray = elemento.innerHTML.split('');
+	elemento.innerHTML = ' ';
+	textoArray.forEach(function (letra, i) {
+
+		setTimeout(function () {
+			elemento.innerHTML += letra;
+		}, 25 * i)
+
+	});
+}
+
+
+typeWrite(titulo);
+
 let theme = localStorage.getItem('theme')
 
-if(theme == null){
+if (theme == null) {
 	setTheme('light')
-}else{
+} else {
 	setTheme(theme)
 }
 
 let temas = document.getElementsByClassName('theme-dot')
 
-for (var i=0; temas.length > i; i++){
-	temas[i].addEventListener('click', function(){
+for (var i = 0; temas.length > i; i++) {
+	temas[i].addEventListener('click', function () {
 		let mode = this.dataset.mode
 		console.log('Opções de cliques:', mode)
 		setTheme(mode)
 	})
 }
 
-function setTheme(mode){
-	if(mode == 'light'){
+function setTheme(mode) {
+	if (mode == 'light') {
 		document.getElementById('theme-style').href = 'default.css'
 	}
 
-	if(mode == 'blue'){
+	if (mode == 'blue') {
 		document.getElementById('theme-style').href = 'blue.css'
 	}
 
-	if(mode == 'green'){
+	if (mode == 'green') {
 		document.getElementById('theme-style').href = 'green.css'
 	}
 
-	if(mode == 'purple'){
+	if (mode == 'purple') {
 		document.getElementById('theme-style').href = 'purple.css'
 	}
 
