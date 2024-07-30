57c2df8a5bc3a999bcc3a043ebab0d8e16074980
João Vitor <joaovitorferreiragoncalves3691@gmail.com>
2024-07-30 18:10:45 -0300

    Correção dos temas

--- script.js
+++ script.js
@@ -1,85 +1,77 @@
-console.log('Estamos trabalhando...')
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Estamos trabalhando...')
 
-mudanca = true
+    let mudanca = true
 
-const descricao = document.getElementById('acao');
-let checkbox = document.getElementById('checkboxID')
+    const descricao = document.getElementById('acao');
+    let checkbox = document.getElementById('checkboxID')
 
+    checkbox.addEventListener('click', verificarTrueOrFalse)
 
-checkbox.addEventListener('click', verificarTrueOrFalse)
+    verificar()
 
-verificar()
+    function verificarTrueOrFalse(){
+        mudanca = !mudanca; // Alterna o valor de 'mudanca'
+        verificar()
+        console.log(mudanca)
+    }
 
-function verificarTrueOrFalse(){
-	if(mudanca == true){
-		mudanca = false
-		verificar()
-		console.log(mudanca)
-	} else if (mudanca == false){
-		mudanca = true
-		verificar()
-		console.log(mudanca)
-	}
-}
+    function verificar() {
+        let apresentacao = document.getElementById('h1_apresentacao');
+        if (mudanca) {
+            apresentacao.innerHTML = 'Olá, meu nome é João Vitor <br> e sou desenvolvedor Back-End';
+        } else {
+            apresentacao.innerHTML = 'Olá, meu nome é João Vitor <br> e sou Analista de TI'
+        }
+    }
 
-function verificar() {
-	let apresentacao = document.getElementById('h1_apresentacao');
-	if (mudanca == true) {
-		apresentacao.innerHTML = 'Olá, meu nome é João Vitor <br> e sou desenvolvedor Back-End';
-	} else if (mudanca == false){
-		apresentacao.innerHTML = 'Olá, meu nome é João Vitor <br> e sou Analista de TI'
-	}
-}
+    function typeWrite(elemento) {
+        const textoArray = elemento.innerHTML.split(''); // Corrige 'inner' para 'innerHTML'
+        elemento.innerHTML = ' ';
+        textoArray.forEach(function (letra, i) {
+            setTimeout(function () {
+                elemento.innerHTML += letra;
+            }, 25 * i)
+        });
+    }
 
-function typeWrite(elemento) {
-	const textoArray = elemento.inner.split('');
-	elemento.innerHTML = ' ';
-	textoArray.forEach(function (letra, i) {
+    typeWrite(descricao);
 
-		setTimeout(function () {
-			elemento.innerHTML += letra;
-		}, 25 * i)
+    let theme = localStorage.getItem('theme')
 
-	});
-}
+    if (theme == null) {
+        setTheme('light')
+    } else {
+        setTheme(theme)
+    }
 
+    let temas = document.getElementsByClassName('theme-dot')
 
-typeWrite(descricao);
+    for (var i = 0; temas.length > i; i++) {
+        temas[i].addEventListener('click', function () {
+            let mode = this.dataset.mode
+            console.log('Opções de cliques:', mode)
+            setTheme(mode)
+        })
+    }
 
-let theme = localStorage.getItem('theme')
+    function setTheme(mode) {
+        const themeStyle = document.getElementById('theme-style');
+        if (!themeStyle) {
+            console.error('Elemento de estilo de tema não encontrado!');
+            return;
+        }
 
-if (theme == null) {
-	setTheme('light')
-} else {
-	setTheme(theme)
-}
+        if (mode == 'light') {
+            themeStyle.href = 'default.css'
+        } else if (mode == 'blue') {
+            themeStyle.href = 'blue.css'
+        } else if (mode == 'green') {
+            themeStyle.href = 'green.css'
+        } else if (mode == 'purple') {
+            themeStyle.href = 'purple.css'
+        }
 
-let temas = document.getElementsByClassName('theme-dot')
-
-for (var i = 0; temas.length > i; i++) {
-	temas[i].addEventListener('click', function () {
-		let mode = this.dataset.mode
-		console.log('Opções de cliques:', mode)
-		setTheme(mode)
-	})
-}
-
-function setTheme(mode) {
-	if (mode == 'light') {
-		document.getElementById('theme-style').href = 'default.css'
-	}
-
-	if (mode == 'blue') {
-		document.getElementById('theme-style').href = 'blue.css'
-	}
-
-	if (mode == 'green') {
-		document.getElementById('theme-style').href = 'green.css'
-	}
-
-	if (mode == 'purple') {
-		document.getElementById('theme-style').href = 'purple.css'
-	}
-
-	localStorage.setItem('theme', mode)
-}
+        localStorage.setItem('theme', mode)
+    }
+});
